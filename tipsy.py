780d6c71dc88ac661fb44e85f7b89cbd9bd9dda0
Sonya Eick <llkelly88@gmail.com>
2012-10-25 10:39:18 -0700

    adding templates for html

--- tipsy.py
+++ tipsy.py
@@ -1,10 +1,16 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, g
 import model
+#g is a special global variable that lets us have multiple users signed on at the same time
 
 app = Flask(__name__)
+
+#This will be executed every time before any view is called
+@app.before_request
+def set_up_db():
+    g.db = model.connect_db()
 
 @app.route("/")
 def index():
@@ -12,28 +18,29 @@ def index():
 
 @app.route("/save_task", methods=["POST"])
 def save_task():
-    db = model.connect_db()
     title = request.form['title']
-    model.new_task(db, title)
+    model.new_task(g.db, title)
     return redirect("/tasks")
 
 @app.route("/tasks")
 def list_tasks():
-    db = model.connect_db()
-    tasks_from_db = model.get_tasks(db, None)
+    tasks_from_db = model.get_tasks(g.db, None)
     return render_template("list_tasks.html", tasks=tasks_from_db)
 
 @app.route("/task/<int:id>", methods=["GET"])
 def view_task(id):
-    db = model.connect_db()
-    task_from_db = model.get_task(db, id)
+    task_from_db = model.get_task(g.db, id)
     return render_template("view_task.html", task=task_from_db)
 
 @app.route("/task/<int:id>", methods=["POST"])
 def complete_task(id):
-    db = model.connect_db()
-    model.complete_task(db, id)
+    model.complete_task(g.db, id)
     return redirect("/tasks")
+
+#executed after each view; closes connection to our database
+@app.teardown_request
+def disconnect_db(exception):
+    g.db.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
